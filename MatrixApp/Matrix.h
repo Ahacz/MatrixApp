5fbcdd5e4f68fe0e5dbd3737198d9ce177e42fe2
Michal Mazurkiewicz <michal10.95@gmail.com>
2020-09-27 19:04:40 +0200

    Cleaned up

--- MatrixApp/Matrix.h
+++ MatrixApp/Matrix.h
@@ -25,20 +25,9 @@ public:
 	void resize(unsigned, T newValue=0);
 };
 
-class IMatrix { //Trying to apply polymorphism, so that user can specify type of matrix in the main program.
-public:
-	virtual ~IMatrix() {};
-	virtual void print() = 0;
-	//virtual IMatrix& operator=(const IMatrix&) = 0;
-	//virtual IMatrix operator+(const IMatrix&) = 0;
-	//virtual IMatrix operator-(const IMatrix&) = 0;
-	//virtual IMatrix operator*(const IMatrix&) = 0;
-	//virtual IMatrix& operator+=(const IMatrix&) = 0;
-	//virtual IMatrix& operator-=(const IMatrix&) = 0;
-	//virtual IMatrix& operator*=(const IMatrix&) = 0;
-};
 
-template <typename T> class Matrix: public IMatrix
+
+template <typename T> class Matrix
 {
 private:
 	unsigned m_rowSize;
